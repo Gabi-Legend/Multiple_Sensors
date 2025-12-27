b096f0da0a157d08572487919a45fa3e892a5486
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2025-12-27 19:55:30 +0200

    Adding button

--- multiple_sensors.ino
+++ multiple_sensors.ino
@@ -1,12 +1,33 @@
+int button = 6;
 int touch = 7;
 
+int cnt = 0;
+const int totalSensors = 2;
+
+bool buttonProcessed = false;
+bool touchProcessed = false;
 void setup() {
+  pinMode(button, INPUT_PULLUP);
   pinMode(touch, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  int value = digitalRead(touch);
-  Serial.println(value);
-  delay(500);
+  if(!digitalRead(button) && !buttonProcessed){ 
+    cnt++;
+    buttonProcessed = true;
+    Serial.println("Buton apast");
+    delay(200); 
+  }
+  if(digitalRead(touch) && !touchProcessed){ 
+    cnt++;
+    touchProcessed = true;
+    Serial.println("Senzor atins");
+    delay(200);
+  }
+
+  if(cnt == totalSensors){
+    Serial.println("Gata");
+    while(true); 
+  }
 }
