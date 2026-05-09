84531cc2dca800b3c4002bdd321b3fe14ef60938
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2026-05-09 19:55:59 +0300

    Bug fixes

--- multiple_sensors.ino
+++ multiple_sensors.ino
@@ -1,225 +1,433 @@
-int button = 6;
-int touch = 7;
-int laser = 8;
-int redLED = 9;
-int greenLED = 10;
-int blueLED = 11;
-int buzzer = 12;
-int passiveBuzzer = A4; 
-int swRotary = 5;
-int clkRotary = 3;
-int dtRotary = 4;
-int start_reset_button = 2;
+// =========================
+// PIN DEFINITIONS
+// =========================
 
-int cnt = 0;
-int cntSW = 0;
-int cntRotary = 0;
-const int totalSensors = 5;
+const int BUTTON_PIN = 6;
+const int TOUCH_PIN = 7;
+const int LASER_PIN = 8;
+
+const int RED_LED = 9;
+const int GREEN_LED = 10;
+const int BLUE_LED = 11;
+
+const int BUZZER_PIN = 12;
+const int PASSIVE_BUZZER = A4;
+
+const int ROTARY_SW = 5;
+const int ROTARY_CLK = 3;
+const int ROTARY_DT = 4;
+
+const int START_BUTTON = 2;
+const int LIGHT_SENSOR = A5;
+
+// =========================
+// GAME CONFIG
+// =========================
+
+const int TOTAL_SENSORS = 5;
+
+const int LIGHT_THRESHOLD = 400;
+
+const int REQUIRED_ROTATIONS = 3;
+const int REQUIRED_SW_PRESSES = 3;
+
+const unsigned long TIMER_DURATION = 60000;
+
+// =========================
+// GAME STATES
+// =========================
+
+enum GameState {
+  IDLE,
+  PLAYING,
+  WIN_STATE,
+  LOSE_STATE
+};
+
+GameState currentState = IDLE;
+
+// =========================
+// GAME VARIABLES
+// =========================
+
+int progressCount = 0;
+
+int rotaryButtonCount = 0;
+int rotaryRotationCount = 0;
 
 bool buttonProcessed = false;
 bool touchProcessed = false;
 bool lightProcessed = false;
-bool swProcessed = false;
+bool rotarySwitchProcessed = false;
+
 bool lastSWState = HIGH;
 int lastCLKState = LOW;
 
-bool timerRunning = false;
 unsigned long startTime = 0;
-const unsigned long timerDuration = 60000; // 60 secunde
 int lastSecondPrinted = -1;
 
+// =========================
+// SETUP
+// =========================
+
 void setup() {
-  pinMode(button, INPUT_PULLUP);
-  pinMode(touch, INPUT);
-  pinMode(laser, OUTPUT);
-  pinMode(redLED, OUTPUT);
-  pinMode(greenLED, OUTPUT);
-  pinMode(blueLED, OUTPUT);
-  pinMode(buzzer, OUTPUT);
-  pinMode(passiveBuzzer, OUTPUT);
-  pinMode(swRotary, INPUT_PULLUP);
-  pinMode(clkRotary, INPUT);
-  pinMode(dtRotary, INPUT);
-  pinMode(start_reset_button, INPUT_PULLUP);
+
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(TOUCH_PIN, INPUT);
+
+  pinMode(LASER_PIN, OUTPUT);
+
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
+
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(PASSIVE_BUZZER, OUTPUT);
+
+  pinMode(ROTARY_SW, INPUT_PULLUP);
+  pinMode(ROTARY_CLK, INPUT);
+  pinMode(ROTARY_DT, INPUT);
+
+  pinMode(START_BUTTON, INPUT_PULLUP);
+
   Serial.begin(9600);
-  lastCLKState = digitalRead(clkRotary);
+
+  lastCLKState = digitalRead(ROTARY_CLK);
+
+  resetGame();
 }
 
-void resetLabirint() {
-  cnt = 0;
-  cntSW = 0;
-  cntRotary = 0;
+// =========================
+// RESET GAME
+// =========================
+
+void resetGame() {
+
+  progressCount = 0;
+
+  rotaryButtonCount = 0;
+  rotaryRotationCount = 0;
+
   buttonProcessed = false;
   touchProcessed = false;
   lightProcessed = false;
-  swProcessed = false;
-  timerRunning = false;
+  rotarySwitchProcessed = false;
+
+  digitalWrite(LASER_PIN, LOW);
+
+  analogWrite(RED_LED, 0);
+  analogWrite(GREEN_LED, 0);
+  analogWrite(BLUE_LED, 0);
+
+  digitalWrite(BUZZER_PIN, LOW);
+
+  noTone(PASSIVE_BUZZER);
+
   lastSecondPrinted = -1;
-  
-  digitalWrite(laser, LOW);
-  analogWrite(redLED, 0);
-  analogWrite(greenLED, 0);
-  analogWrite(blueLED, 0);
-  digitalWrite(buzzer, LOW);
-  noTone(passiveBuzzer);
-  
-  Serial.println("Labirint resetat!");
+
+  currentState = IDLE;
+
+  Serial.println("Game Reset");
 }
+
+// =========================
+// START GAME
+// =========================
+
+void startGame() {
+
+  resetGame();
+
+  startTime = millis();
+
+  currentState = PLAYING;
+
+  Serial.println("GAME STARTED");
+}
+
+// =========================
+// PROGRESS SOUND
+// =========================
 
 void playProgressSound() {
-  int frequencies[] = {262, 330, 392, 494, 587}; 
-  int freq = frequencies[cnt - 1];
-  tone(passiveBuzzer, freq, 300);
+
+  int frequencies[] = {262, 330, 392, 494, 587};
+
+  tone(PASSIVE_BUZZER, frequencies[progressCount - 1], 200);
 }
 
-void loop() {
-  // START / RESET button
-  if(!digitalRead(start_reset_button)) {
-    resetLabirint();
-    startTime = millis();
-    timerRunning = true;
-    Serial.println("JOC INCEPUT");
-    delay(200);  
+// =========================
+// LASER BLINK
+// =========================
+
+void blinkLaser() {
+
+  digitalWrite(LASER_PIN, HIGH);
+  delay(50);
+
+  digitalWrite(LASER_PIN, LOW);
+  delay(50);
+}
+
+// =========================
+// LED FADE
+// =========================
+
+void fadeLED(int red, int green, int blue) {
+
+  analogWrite(RED_LED, red);
+  analogWrite(GREEN_LED, green);
+  analogWrite(BLUE_LED, blue);
+}
+
+// =========================
+// TIMER
+// =========================
+
+void updateTimer() {
+
+  if(currentState != PLAYING) {
+    return;
   }
 
-  // Timer logic
-  if(timerRunning) {
-    unsigned long elapsed = millis() - startTime;
+  unsigned long elapsed = millis() - startTime;
 
-    if(elapsed >= timerDuration) {
-      Serial.println("Timpul a expirat!");
-      digitalWrite(buzzer, HIGH);
-      delay(1000);
-      digitalWrite(buzzer, LOW);
-      resetLabirint();
-    } else if (elapsed >= 55000) { // ultimele 5 secunde
-      int secLeft = 60 - (elapsed / 1000);
-      if(secLeft != lastSecondPrinted) {
-        Serial.println(secLeft);
-        lastSecondPrinted = secLeft;
-      }
+  if(elapsed >= TIMER_DURATION) {
+
+    currentState = LOSE_STATE;
+
+    Serial.println("TIME EXPIRED");
+
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(1000);
+    digitalWrite(BUZZER_PIN, LOW);
+
+    resetGame();
+  }
+
+  else if(elapsed >= 55000) {
+
+    int secLeft = 60 - (elapsed / 1000);
+
+    if(secLeft != lastSecondPrinted) {
+
+      Serial.print("Seconds left: ");
+      Serial.println(secLeft);
+
+      lastSecondPrinted = secLeft;
     }
   }
+}
 
-  // Button
-  if(!digitalRead(button) && !buttonProcessed){ 
-    cnt++;
+// =========================
+// INPUTS
+// =========================
+
+void handleInputs() {
+
+  if(currentState != PLAYING) {
+    return;
+  }
+
+  // BUTTON
+
+  if(!digitalRead(BUTTON_PIN) && !buttonProcessed) {
+
+    progressCount++;
+
     buttonProcessed = true;
-    Serial.println("Buton apasat");
+
+    Serial.println("Button pressed");
+
     playProgressSound();
-    delay(200); 
+
+    delay(150);
   }
 
-  // Touch sensor
-  if(digitalRead(touch) && !touchProcessed){ 
-    cnt++;
+  // TOUCH SENSOR
+
+  if(digitalRead(TOUCH_PIN) && !touchProcessed) {
+
+    progressCount++;
+
     touchProcessed = true;
-    Serial.println("Senzor atins");
+
+    Serial.println("Touch detected");
+
     playProgressSound();
-    delay(200);
+
+    delay(150);
   }
 
-  // Light sensor
-  if(analogRead(A5)<400 && !lightProcessed){
-    cnt++;
+  // LIGHT SENSOR
+
+  if(analogRead(LIGHT_SENSOR) < LIGHT_THRESHOLD && !lightProcessed) {
+
+    progressCount++;
+
     lightProcessed = true;
-    Serial.println("Lumina inchisa");
+
+    Serial.println("Light blocked");
+
     playProgressSound();
-    delay(200);
+
+    delay(150);
   }
 
-  // Rotary encoder SW
-  bool currentSWState = digitalRead(swRotary);
-  if(lastSWState == HIGH && currentSWState == LOW && !swProcessed){
-    cntSW++;
-    Serial.println("SW Apasat");
-    if(cntSW == 3){
-      cnt++;
-      swProcessed = true;
+  // ROTARY SWITCH
+
+  bool currentSWState = digitalRead(ROTARY_SW);
+
+  if(lastSWState == HIGH &&
+     currentSWState == LOW &&
+     !rotarySwitchProcessed) {
+
+    rotaryButtonCount++;
+
+    Serial.print("Rotary button count: ");
+    Serial.println(rotaryButtonCount);
+
+    if(rotaryButtonCount >= REQUIRED_SW_PRESSES) {
+
+      progressCount++;
+
+      rotarySwitchProcessed = true;
+
       playProgressSound();
     }
+
     delay(50);
   }
+
   lastSWState = currentSWState;
 
-  // Rotary encoder rotation
-  int currentCLKState = digitalRead(clkRotary);
-  if(currentCLKState != lastCLKState){ 
-    if(digitalRead(dtRotary) != currentCLKState){ 
-      cntRotary++;
-      Serial.print("Ai rotit in dreapta: ");
-      Serial.println(cntRotary);
-      if(cntRotary == 3){
-        cnt++;
-        cntRotary = 0; 
+  // ROTARY ROTATION
+
+  int currentCLKState = digitalRead(ROTARY_CLK);
+
+  if(currentCLKState != lastCLKState) {
+
+    if(digitalRead(ROTARY_DT) != currentCLKState) {
+
+      rotaryRotationCount++;
+
+      Serial.print("Rotations: ");
+      Serial.println(rotaryRotationCount);
+
+      if(rotaryRotationCount >= REQUIRED_ROTATIONS) {
+
+        progressCount++;
+
+        rotaryRotationCount = 0;
+
         playProgressSound();
       }
     }
   }
-  lastCLKState = currentCLKState;
 
-  // Labirint complet
-  if(cnt == totalSensors){
-    timerRunning = false; 
-    noTone(passiveBuzzer); 
-    Serial.println("FELICITARI! AI CASTIGAT!");
-    
-    for(int repeat = 0; repeat < 3; repeat++) {
-      digitalWrite(buzzer, HIGH);
-      delay(200);
-      digitalWrite(buzzer,LOW);
-      analogWrite(greenLED, 0);
-      analogWrite(blueLED, 0);
-      for(int i = 20; i <= 240; i+=40){
-        analogWrite(redLED, i);
-        digitalWrite(laser, HIGH);
-        delay(50);
-        digitalWrite(laser, LOW);
-        delay(50);
-      }
-      for(int i = 240; i >= 20; i-=40){
-        analogWrite(redLED, i);
-        digitalWrite(laser, HIGH);
-        delay(50);
-        digitalWrite(laser, LOW);
-        delay(50);
-      }
-      
-      // Verde
-      analogWrite(redLED, 0);
-      analogWrite(blueLED, 0);
-      for(int i = 20; i <= 240; i+=40){
-        analogWrite(greenLED, i);
-        digitalWrite(laser, HIGH);
-        delay(50);
-        digitalWrite(laser, LOW);
-        delay(50);
-      }
-      for(int i = 240; i >= 20; i-=40){
-        analogWrite(greenLED, i);
-        digitalWrite(laser, HIGH);
-        delay(50);
-        digitalWrite(laser, LOW);
-        delay(50);
-      }
-      
-      // Albastru
-      analogWrite(redLED, 0);
-      analogWrite(greenLED, 0);
-      for(int i = 20; i <= 240; i+=40){
-        analogWrite(blueLED, i);
-        digitalWrite(laser, HIGH);
-        delay(50);
-        digitalWrite(laser, LOW);
-        delay(50);
-      }
-      for(int i = 240; i >= 20; i-=40){
-        analogWrite(blueLED, i);
-        digitalWrite(laser, HIGH);
-        delay(50);
-        digitalWrite(laser, LOW);
-        delay(50);
-      }
+  lastCLKState = currentCLKState;
+}
+
+// =========================
+// WIN CONDITION
+// =========================
+
+void checkWinCondition() {
+
+  if(progressCount >= TOTAL_SENSORS) {
+
+    currentState = WIN_STATE;
+
+    Serial.println("YOU WIN!");
+
+    playWinAnimation();
+
+    resetGame();
+  }
+}
+
+// =========================
+// WIN ANIMATION
+// =========================
+
+void playWinAnimation() {
+
+  for(int repeat = 0; repeat < 3; repeat++) {
+
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(150);
+    digitalWrite(BUZZER_PIN, LOW);
+
+    // RED
+
+    for(int i = 20; i <= 255; i += 40) {
+
+      fadeLED(i, 0, 0);
+
+      blinkLaser();
+    }
+
+    for(int i = 255; i >= 20; i -= 40) {
+
+      fadeLED(i, 0, 0);
+
+      blinkLaser();
+    }
+
+    // GREEN
+
+    for(int i = 20; i <= 255; i += 40) {
+
+      fadeLED(0, i, 0);
+
+      blinkLaser();
+    }
+
+    for(int i = 255; i >= 20; i -= 40) {
+
+      fadeLED(0, i, 0);
+
+      blinkLaser();
+    }
+
+    // BLUE
+
+    for(int i = 20; i <= 255; i += 40) {
+
+      fadeLED(0, 0, i);
+
+      blinkLaser();
+    }
+
+    for(int i = 255; i >= 20; i -= 40) {
+
+      fadeLED(0, 0, i);
+
+      blinkLaser();
     }
   }
+
+  fadeLED(0, 0, 0);
+}
+
+// =========================
+// MAIN LOOP
+// =========================
+
+void loop() {
+
+  // START BUTTON
+
+  if(!digitalRead(START_BUTTON)) {
+
+    startGame();
+
+    delay(200);
+  }
+
+  updateTimer();
+
+  handleInputs();
+
+  checkWinCondition();
 }
